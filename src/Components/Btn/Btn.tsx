8c50753abe2c75b370cd7eccead6ad0d2928f517
Vito Medlej <rip.vitto@gmail.com>
2023-03-29 14:09:23 +0300

    minor changes

--- src/Components/Btn/Btn.tsx
+++ src/Components/Btn/Btn.tsx
@@ -2,20 +2,32 @@ import { Button } from '@mui/material'
 import React from 'react'
 
 
-const styles ={
-    borderRadius:0,
+const styles =(dark?:boolean) => {
+  let a;
+  dark ? a= {
+    border:'1px solid black',
+    background:'transparent',
+    color:'black',
+    px:'3.5em',
+    py:'.6em',
+  }
+  :
+  a= {
+    
     border:'1px solid white',
     background:'transparent',
     color:'white',
     px:'3.5em',
     py:'.6em',
+  }
+  return a
 }
-const Btn = ({children,blur,sx}:{blur?:boolean,sx?:any,children?:any}) => {
+const Btn = ({dark,children,blur,sx}:{dark?:boolean,blur?:boolean,sx?:any,children?:any}) => {
   return (
     <Button 
     className={`${blur ? 'blur' :'' }`}
     sx={{
-        ...styles,
+     ...styles(dark),
         ...sx
     }}>{children}</Button>
   )
