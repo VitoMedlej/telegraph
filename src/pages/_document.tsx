ce5e02e448013696a4e4651b9c639617147dc456
vito medlej <vito.medlej@gmail.com>
2023-06-14 18:54:38 +0300

    verify google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <meta name="google-site-verification" content="77Hycj7z6DYVLMtxlqqTMqwRAH4cfmOL55pOOLgKr7c" />
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
     <link rel="icon" href="https://ucarecdn.com/3d4abdb1-ac77-45ea-9fc2-30f85a0e1b06/obiconv3.JPG" />
